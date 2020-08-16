1b797a6d0863c0899ab3a7442517bbf252748224
github-action-benchmark <github@users.noreply.github.com>
2020-08-16 06:46:54 +0000

    add Python Benchmark with pytest-benchmark benchmark result for 31d0f60a74cacce545df6e4f5a4f5c505b2b7df2

---
+++ dev/bench/data.js
@@ -0,0 +1,51 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1597560414016,
+  "repoUrl": "https://github.com/chrisjsewell/github-action-benchmark",
+  "entries": {
+    "Python Benchmark with pytest-benchmark": [
+      {
+        "cpu": {
+          "speed": "2.30",
+          "cores": 2,
+          "physicalCores": 2,
+          "processors": 1
+        },
+        "commit": {
+          "author": {
+            "email": "chrisj_sewell@hotmail.com",
+            "name": "Chris Sewell",
+            "username": "chrisjsewell"
+          },
+          "committer": {
+            "email": "chrisj_sewell@hotmail.com",
+            "name": "Chris Sewell",
+            "username": "chrisjsewell"
+          },
+          "distinct": true,
+          "id": "31d0f60a74cacce545df6e4f5a4f5c505b2b7df2",
+          "message": "Update ci.yml",
+          "timestamp": "2020-08-16T07:45:50+01:00",
+          "tree_id": "e9561e502f6cbabf497924d4a33bef6198a2ab28",
+          "url": "https://github.com/chrisjsewell/github-action-benchmark/commit/31d0f60a74cacce545df6e4f5a4f5c505b2b7df2"
+        },
+        "date": 1597560412213,
+        "benches": [
+          {
+            "name": "bench.py::test_fib_10",
+            "value": 39040.03287564513,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0005193064798077989",
+            "extra": "mean: 25.614732528154292 usec\nrounds: 40651"
+          },
+          {
+            "name": "bench.py::test_fib_20",
+            "value": 343.560637250009,
+            "unit": "iter/sec",
+            "range": "stddev: 0.003922164653719965",
+            "extra": "mean: 2.910694333333362 msec\nrounds: 378"
+          }
+        ]
+      }
+    ]
+  }
+}
