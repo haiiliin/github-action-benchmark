f56c9e1c8265d7f187c1490e960483de6fdeffca
github-action-benchmark <github@users.noreply.github.com>
2020-08-16 06:48:30 +0000

    add Python Benchmark with pytest-benchmark benchmark result for a8df1901b67b94ddcf2d68ac636292f8dacc3a73

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1597560414016,
+  "lastUpdate": 1597560510237,
   "repoUrl": "https://github.com/chrisjsewell/github-action-benchmark",
   "entries": {
     "Python Benchmark with pytest-benchmark": [
@@ -43,6 +43,49 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.003922164653719965",
             "extra": "mean: 2.910694333333362 msec\nrounds: 378"
+          }
+        ]
+      },
+      {
+        "cpu": {
+          "speed": "2.30",
+          "cores": 2,
+          "physicalCores": 2,
+          "processors": 1
+        },
+        "commit": {
+          "author": {
+            "email": "chrisj_sewell@hotmail.com",
+            "name": "Chris Sewell",
+            "username": "chrisjsewell"
+          },
+          "committer": {
+            "email": "chrisj_sewell@hotmail.com",
+            "name": "Chris Sewell",
+            "username": "chrisjsewell"
+          },
+          "distinct": true,
+          "id": "a8df1901b67b94ddcf2d68ac636292f8dacc3a73",
+          "message": "Update README.md",
+          "timestamp": "2020-08-16T07:48:02+01:00",
+          "tree_id": "e6dbebb58a2251ffd79c35e50a45e1256fe3e885",
+          "url": "https://github.com/chrisjsewell/github-action-benchmark/commit/a8df1901b67b94ddcf2d68ac636292f8dacc3a73"
+        },
+        "date": 1597560508894,
+        "benches": [
+          {
+            "name": "bench.py::test_fib_10",
+            "value": 58761.67294656083,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000004599904358130771",
+            "extra": "mean: 17.017895336462292 usec\nrounds: 22472"
+          },
+          {
+            "name": "bench.py::test_fib_20",
+            "value": 476.4639196390308,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0003058035662843394",
+            "extra": "mean: 2.098794806451662 msec\nrounds: 496"
           }
         ]
       }
